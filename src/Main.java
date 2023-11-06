8f3b69db0f7c63a5628cb1d3c7c58f713f46a68f
Nurmuhammad <snurnuhammad85@icloud.com>
2023-11-06 17:34:03 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    static {
+        try {
+
+            FileHandler fileHandler = new FileHandler("output/logs",true);
+            SimpleFormatter simpleFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(simpleFormatter);
+            log.addHandler(fileHandler);
+
+        } catch (IOException e) {
+
+            log.warning(e.getMessage());
+
+        }
+    }
+
     public static void main(String[] args) {
+
+        EmailAnalyseService emailAnalyseService = new EmailAnalyseService();
+
+        ArrayList<Email> emailList = emailAnalyseService.getEmails("input/emails.txt");
+
+        StringBuilder analysis = emailAnalyseService.analysis(emailList);
+
+        FileWriter fileWriter = null;
+        try {
+
+            fileWriter = new FileWriter("output/e-mail_analysis_report.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(emailAnalyseService.analysis(emailList).toString());
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+          log.warning(e.getMessage());
+        }
+
+
+        for (Email email : emailList) {
+            System.out.println(email);
+        }
 
     }
 }
